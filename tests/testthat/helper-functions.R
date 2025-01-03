7b6019154831e0b18c4159be48af3d639287033b
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-01-03 12:35:53 -0500

    test: use all.equal for more lenient df comparison
    tolerance out to 4 decimals is plenty of precision

--- tests/testthat/helper-functions.R
+++ tests/testthat/helper-functions.R
@@ -2,7 +2,7 @@ equal_dfs <- function(x, y) {
   all(
     class(x) == class(y),
     names(x) == names(y),
-    x == y,
+    all.equal(x, y),
     all.equal(lapply(x, class), lapply(y, class))
   )
 }
