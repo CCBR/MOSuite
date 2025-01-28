15c2d1cb7d463633e0e05be5ed3210271a5bdc94
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-01-28 16:58:54 -0500

    docs: fix typo in dataset; document anno_dat

--- R/0_mo-class.R
+++ R/0_mo-class.R
@@ -2,6 +2,7 @@
 #'
 #' @param sample_meta_dat sample metadata as a data frame or tibble.
 #'   The first column is assumed to contain the sample IDs which must correspond to column names in the raw counts.
+#' @param anno_dat data frame of feature annotations, such as gene symbols or any other information about the features in `counts_lst`.
 #' @param counts_lst named list of data frames containing counts, e.g. expected feature counts from RSEM.
 #'   Each data frame is expected to contain a `feature_id` column as the first column, and all remaining columns are sample IDs in the `sample_meta`.
 #' @param analyses_lst named list of analysis results, e.g. DESeq results object
@@ -135,7 +136,7 @@ create_multiOmicDataSet_from_dataframes <- function(sample_meta_dat,
 #'     package = "MOSuite"
 #'   ),
 #'   feature_counts_filepath = system.file("extdata",
-#'     "RSEM.features.expected_count.all_samples.txt.gz",
+#'     "RSEM.genes.expected_count.all_samples.txt.gz",
 #'     package = "MOSuite"
 #'   )
 #' )
