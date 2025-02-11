32ff66adcd23a40d9adf36b84a049aa7c1e2d631
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-02-11 13:53:28 -0500

    chore: try setting default option functions
    https://dgkf.github.io/options/articles/envvars.html\#setting-naming-rules

--- R/options.R
+++ R/options.R
@@ -1,11 +1,19 @@
+options::set_option_name_fn(function(package, name) {
+  tolower(paste0("moo_", name))
+})
+
+options::set_envvar_name_fn(function(package, name) {
+  gsub("[^A-Z0-9]", "_", toupper(paste0("moo_", name)))
+})
+
 options::define_option(
   option = "print_plots",
-  desc = "Whether to print plots during analysis",
   default = FALSE,
+  desc = "Whether to print plots during analysis",
   option_name = "moo_print_plots",
-  envvar_name = "MOO_PRINT_PLOTS",
-  envvar_fn = options::envvar_is_true()
+  envvar_name = "MOO_PRINT_PLOTS"
 )
+
 
 #' @eval options::as_roxygen_docs()
 NULL
