09f4002c533968438c6b463125214637714bff91
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-02-11 13:42:32 -0500

    fix: options usage
    prepend option name with MOO

--- R/options.R
+++ R/options.R
@@ -1,6 +1,10 @@
-options::define_options(
-  "Whether to print plots during analysis",
-  print_plots = TRUE
+options::define_option(
+  option = "print_plots",
+  desc = "Whether to print plots during analysis",
+  default = FALSE,
+  option_name = "moo_print_plots",
+  envvar_name = "MOO_PRINT_PLOTS",
+  envvar_fn = options::envvar_is_true()
 )
 
 #' @eval options::as_roxygen_docs()
