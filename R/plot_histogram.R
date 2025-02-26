9b1c075822d6992401ef97a1ec19401cbad45302
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-02-26 11:15:57 -0500

    refactor: structure plot_histogram as generic + 2 methods
    took some trial and error to figure out how to document with roxygen
    
    unfortunately, inheritParams doesn not work as it does not recognize methods as functions

--- R/plot_histogram.R
+++ R/plot_histogram.R
@@ -1,44 +1,165 @@
 #' Plot histogram
 #'
-#' @inheritParams create_multiOmicDataSet_from_dataframes
-#' @inheritParams filter_counts
+#' The first argument can be a `multiOmicDataset` object (`moo`) or a `data.frame` containing counts.
+#' For a `moo`, choose which counts slot to use with `count_type` & `sub_count_type`.
 #'
-#' @param x_axis_label text label for the x axis `ggplot2::xlab()`
-#' @param y_axis_label text label for the y axis `ggplot2::ylab()`
+#' # Methods
+#'
+#'  See documentation below for method-specific for arguments
+#'
+#'   | method                   | class of `moo_counts` |
+#'   |--------------------------|--------------------|
+#'   | [plot_histogram_moo]     | `multiOmicDataset` |
+#'   | [plot_histogram_dat]     | `data.frame`       |
+#'
+#' @param moo_counts counts dataframe or `multiOmicDataSet` containing `count_type` & `sub_count_type` in the counts slot
+#' @param ... remaining arguments are forwarded to the method
+#'
+#' @returns ggplot object
+#' @export
+#'
+#' @examples
+#' # plot histogram for a counts slot in a multiOmicDataset Object
+#' moo <- multiOmicDataSet(
+#'   sample_metadata = nidap_sample_metadata,
+#'   anno_dat = data.frame(),
+#'   counts_lst = list("raw" = nidap_raw_counts)
+#' )
+#' p <- plot_histogram(moo, count_type = "raw")
+#'
+#' # plot histogram for a counts dataframe
+#' counts_dat <- moo@counts$raw
+#' plot_histogram(
+#'   counts_dat,
+#'   sample_metadata = nidap_sample_metadata,
+#'   sample_id_colname = "Sample",
+#'   feature_id_colname = "Gene",
+#'   group_colname = "Group",
+#'   label_colname = "Label"
+#' )
+#'
+#' @name plot_histogram
+#' @rdname plot_histogram
+#' @family plotters
+plot_histogram <- S7::new_generic("plot_histogram", dispatch_args = "moo_counts")
+
+#' Plot histogram for multiOmicDataSet
+#'
+#' @param moo_counts multiOmicDataSet` containing `count_type` & `sub_count_type` in the counts slot
+#' @param count_type the type of counts to use -- must be a name in the counts slot (`moo@counts`)
+#' @param sub_count_type if `count_type` is a list, specify the sub count type within the list
+#' @param ... remaining arguments forwarded to the `plot_histogram` method for `data.frame`s (see `?plot_histogram_dat`)
+#'
+#' @returns ggplot object
+#' @examples
+#' # plot histogram for a counts slot in a multiOmicDataset Object
+#' moo <- multiOmicDataSet(
+#'   sample_metadata = nidap_sample_metadata,
+#'   anno_dat = data.frame(),
+#'   counts_lst = list("raw" = nidap_raw_counts)
+#' )
+#' p <- plot_histogram(moo, count_type = "raw")
+#' print(p)
+#'
+#' @name plot_histogram_moo
+#' @method plot_histogram multiOmicDataSet
+#' @seealso [plot_histogram] generic
+S7::method(plot_histogram, multiOmicDataSet) <- function(moo_counts,
+                                                         count_type,
+                                                         sub_count_type = NULL,
+                                                         ...) {
+  moo <- moo_counts
+  # select correct counts matrix
+  if (!(count_type %in% names(moo@counts))) {
+    stop(glue::glue("count_type {count_type} not in moo@counts"))
+  }
+  counts_dat <- moo@counts[[count_type]]
+  if (!is.null(sub_count_type)) {
+    if (!(inherits(counts_dat, "list"))) {
+      stop(
+        glue::glue(
+          "{count_type} counts is not a named list. To use {count_type} counts, set sub_count_type to NULL"
+        )
+      )
+    } else if (!(sub_count_type %in% names(counts_dat))) {
+      stop(
+        glue::glue(
+          "sub_count_type {sub_count_type} is not in moo@counts[[{count_type}]]"
+        )
+      )
+    }
+    counts_dat <- moo@counts[[count_type]][[sub_count_type]]
+  }
+  sample_metadata <- moo@sample_meta
+  plot_histogram(counts_dat, sample_metadata = sample_metadata, ...)
+}
+
+#' Plot histogram for counts dataframe
+#'
+#' @param moo_counts counts [data.frame], i.e. from counts slot of a [multiOmicDataSet]
+#' @param sample_metadata sample metadata as a data frame or tibble.
+#' @param sample_id_colname The column from the sample metadata containing the sample names. The names in this column must exactly match the names used as the sample column names of your input Counts Matrix. (Default: `NULL` - first column in the sample metadata will be used.)
+#' @param feature_id_colname The column from the counts dataa containing the Feature IDs (Usually Gene or Protein ID). This is usually the first column of your input Counts Matrix. Only columns of Text type from your input Counts Matrix will be available to select for this parameter. (Default: `NULL` - first column in the counts matrix will be used.)
+#' @param group_colname The column from the sample metadata containing the sample group information. This is usually a column showing to which experimental treatments each sample belongs (e.g. WildType, Knockout, Tumor, Normal, Before, After, etc.).
+#' @param label_colname The column from the sample metadata containing the sample labels as you wish them to appear in the plots produced by this template. This can be the same Sample Names Column. However, you may desire different labels to display on your figure (e.g. shorter labels are sometimes preferred on plots). In that case, select the column with your preferred Labels here. The selected column should contain unique names for each sample. (Default: `NULL` -- `sample_id_colname` will be used.)
 #' @param color_values vector of colors as hex values or names recognized by R
 #' @param color_by_group Set to FALSE to label histogram by Sample Names, or set to TRUE to label histogram by the column you select in the "Group Column Used to Color Histogram" parameter (below). Default is FALSE.
 #' @param set_min_max_for_x_axis whether to override the default for `ggplot2::xlim()` (default: `FALSE`)
 #' @param minimum_for_x_axis value to override default `min` for `ggplot2::xlim()`
 #' @param maximum_for_x_axis value to override default `max` for `ggplot2::xlim()`
+#' @param x_axis_label text label for the x axis `ggplot2::xlab()`
+#' @param y_axis_label text label for the y axis `ggplot2::ylab()`
 #' @param legend_position passed to in `legend.position` `ggplot2::theme()`
 #' @param legend_font_size passed to `ggplot2::element_text()` via `ggplot2::theme()`
 #' @param number_of_legend_columns passed to `ncol` in `ggplot2::guide_legend()`
+#' @param interactive_plots set to TRUE to make the plot interactive with `plotly`, allowing you to hover your mouse over a point or line to view sample information. The similarity heat map will not display if this toggle is set to TRUE. Default is FALSE.
 #'
 #' @returns ggplot object
-#' @export
+#' @examples
+#' plot_histogram(
+#'   moo_counts = nidap_clean_raw_counts,
+#'   sample_metadata = nidap_sample_metadata,
+#'   sample_id_colname = "Sample",
+#'   feature_id_colname = "Gene",
+#'   group_colname = "Group",
+#'   label_colname = "Label"
+#' )
 #'
-plot_histogram <- function(counts_dat,
-                           sample_metadata,
-                           sample_id_colname = NULL,
-                           feature_id_colname = NULL,
-                           group_colname = "Group",
-                           label_colname = "Label",
-                           color_values = c(
-                             "#5954d6", "#e1562c", "#b80058", "#00c6f8", "#d163e6", "#00a76c",
-                             "#ff9287", "#008cf9", "#006e00", "#796880", "#FFA500", "#878500"
-                           ),
-                           color_by_group = FALSE,
-                           set_min_max_for_x_axis = FALSE,
-                           minimum_for_x_axis = -1,
-                           maximum_for_x_axis = 1,
-                           legend_position = "top",
-                           legend_font_size = 10,
-                           number_of_legend_columns = 6,
-                           x_axis_label = "Counts",
-                           y_axis_label = "Density",
-                           interactive_plots = FALSE) {
+#' @name plot_histogram_dat
+#' @method plot_histogram data.frame
+#' @seealso [plot_histogram] generic
+S7::method(plot_histogram, S7::class_data.frame) <- function(moo_counts,
+                                                             sample_metadata,
+                                                             sample_id_colname = NULL,
+                                                             feature_id_colname = NULL,
+                                                             group_colname = "Group",
+                                                             label_colname = "Label",
+                                                             color_values = c(
+                                                               "#5954d6",
+                                                               "#e1562c",
+                                                               "#b80058",
+                                                               "#00c6f8",
+                                                               "#d163e6",
+                                                               "#00a76c",
+                                                               "#ff9287",
+                                                               "#008cf9",
+                                                               "#006e00",
+                                                               "#796880",
+                                                               "#FFA500",
+                                                               "#878500"
+                                                             ),
+                                                             color_by_group = FALSE,
+                                                             set_min_max_for_x_axis = FALSE,
+                                                             minimum_for_x_axis = -1,
+                                                             maximum_for_x_axis = 1,
+                                                             x_axis_label = "Counts",
+                                                             y_axis_label = "Density",
+                                                             legend_position = "top",
+                                                             legend_font_size = 10,
+                                                             number_of_legend_columns = 6,
+                                                             interactive_plots = FALSE) {
   count <- NULL
-
+  counts_dat <- moo_counts
   if (is.null(sample_id_colname)) {
     sample_id_colname <- colnames(sample_metadata)[1]
   }
@@ -47,7 +168,8 @@ plot_histogram <- function(counts_dat,
   }
 
   df_long <- counts_dat %>%
-    tidyr::pivot_longer(-tidyselect::all_of(feature_id_colname),
+    tidyr::pivot_longer(
+      -tidyselect::all_of(feature_id_colname),
       names_to = sample_id_colname,
       values_to = "count"
     ) %>%
@@ -72,7 +194,10 @@ plot_histogram <- function(counts_dat,
 
     # plot Density
     hist_plot <- df_long %>%
-      ggplot2::ggplot(ggplot2::aes(x = count, group = !!rlang::sym(sample_id_colname))) +
+      ggplot2::ggplot(ggplot2::aes(
+        x = count,
+        group = !!rlang::sym(sample_id_colname)
+      )) +
       ggplot2::geom_density(ggplot2::aes(colour = !!rlang::sym(group_colname)), linewidth = 1)
   } else {
     n <- df_long %>%
@@ -81,7 +206,10 @@ plot_histogram <- function(counts_dat,
       length()
 
     hist_plot <- df_long %>%
-      ggplot2::ggplot(ggplot2::aes(x = count, group = !!rlang::sym(sample_id_colname))) +
+      ggplot2::ggplot(ggplot2::aes(
+        x = count,
+        group = !!rlang::sym(sample_id_colname)
+      )) +
       ggplot2::geom_density(ggplot2::aes(colour = !!rlang::sym(sample_id_colname)), linewidth = 1)
   }
 
@@ -96,7 +224,11 @@ plot_histogram <- function(counts_dat,
       panel.background = ggplot2::element_blank(),
       axis.text = ggplot2::element_text(size = 18),
       axis.title = ggplot2::element_text(size = 20),
-      panel.border = ggplot2::element_rect(colour = "black", fill = NA, linewidth = 0),
+      panel.border = ggplot2::element_rect(
+        colour = "black",
+        fill = NA,
+        linewidth = 0
+      ),
       axis.line = ggplot2::element_line(linewidth = .5),
       axis.ticks = ggplot2::element_line(linewidth = 1)
     ) +
