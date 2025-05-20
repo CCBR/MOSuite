faa12016ab5cfd57f48f25b99029726fd1b87b16
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-05-20 14:28:49 -0400

    chore: recompress data with tools::resaveRdaFiles("data")

--- data-raw/nidap.R
+++ data-raw/nidap.R
@@ -79,3 +79,6 @@ moo <- multiOmicDataSet(
   )
 nidap_deg_analysis_2 <- moo@analyses$diff
 usethis::use_data(nidap_deg_analysis_2, overwrite = TRUE)
+
+## re-compress Rda files
+# tools::resaveRdaFiles('data')
