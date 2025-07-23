57e313afb6b2bbdf3688a7fb138937f279ac76df
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-07-23 13:21:02 -0400

    feat: proof-of-concept report, function, & executable

--- R/render_report.R
+++ R/render_report.R
@@ -1,14 +1,14 @@
 #' Render the template report
 #'
 #' @param input Path to the Quarto report file (default is the template report in the package).
-#' @param ... Additional arguments passed to `quarto::quarto_render()`.
+#' @param ... Additional arguments passed to `quarto::quarto_render()`, such as a named list of parameters.
 #'
 #' @export
 #'
 #' @example
-#' render_report(params = list(counts_csv = system.file("extdata", "RSEM.genes.expected_count.all_samples.txt.gz", package = "MOSuite"), samplesheet_csv = system.file("extdata", "sample_metadata.tsv.gz", package = "MOSuite")))
+#' render_report(execute_params = list(counts_tsv = system.file("extdata", "RSEM.genes.expected_count.all_samples.txt.gz", package = "MOSuite"), samplesheet_tsv = system.file("extdata", "sample_metadata.tsv.gz", package = "MOSuite")))
 render_report <- function(
-    input = system.file("inst/quarto/report.qmd", package = "MOSuite"),
+    input = system.file("quarto", "report.qmd", package = "MOSuite"),
     ...) {
   abort_packages_not_installed(c("quarto", "knitr", "rmarkdown"))
   quarto::quarto_render(
