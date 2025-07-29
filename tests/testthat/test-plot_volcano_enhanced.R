61825e3ed5d7c9987781c3f2b66cff912c12efcf
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-07-29 14:11:33 -0400

    test: improve snapshots for volcano plots

--- tests/testthat/test-plot_volcano_enhanced.R
+++ tests/testthat/test-plot_volcano_enhanced.R
@@ -1,5 +1,5 @@
 test_that("plot_volcano_enhanced works on nidap dataset", {
   expect_snapshot(
-    df_volc_enh <- plot_volcano_enhanced(nidap_deg_analysis, save_plots = TRUE)
+    df_volc_enh <- plot_volcano_enhanced(nidap_deg_analysis, save_plots = FALSE, print_plots = FALSE)
   )
 })
