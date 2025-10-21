931bda48e3270c1879a4e943cfa14ab70880372b
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-10-21 06:58:56 -0400

    ci: use r-lib/setup-r-dependencies then downgrade ggplot2

--- .github/install.R
+++ .github/install.R
@@ -1,5 +1,3 @@
 #!/usr/bin/env Rscript --vanilla
 options(repos = c(CRAN = "https://cloud.r-project.org"))
-install.packages("remotes")
 remotes::install_version("ggplot2", version = "3.5.2")
-remotes::install_local(dependencies = TRUE, upgrade = "never")
