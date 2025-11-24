8b8f88abb9b2ad348e47749a9b052e8df995b1b0
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-11-24 18:06:23 -0500

    feat: fix code templates for first 2 galaxy functions

--- R/galaxy.R
+++ R/galaxy.R
@@ -50,34 +50,41 @@ get_function_meta <- function(func_name, rd_db) {
 #'   tools::Rd_db("MOSuite")
 #' )
 #'
-update_function_template <- function(template_filename, rd_db) {
+update_function_template <- function(template_filename, rd_db, keep_deprecated_args = TRUE) {
   template <- jsonlite::read_json(template_filename)
   r_function <- template$r_function
-  message(
-    glue::glue(
-      "Processing template {basename(template_filename)} for R function {r_function}"
-    )
-  )
-
   func_meta <- get_function_meta(r_function, rd_db)
-  template$description <- func_meta$description
+  new_template <- list(
+    r_function = r_function,
+    title = template$title,
+    description = func_meta$description,
+    columns = list(),
+    inputDatasets = list(),
+    parameters = list()
+  )
   args_in_template <- c()
+  template_args_missing <- c()
   for (arg_type in c("columns", "inputDatasets", "parameters")) {
     for (i in seq_along(template[[arg_type]])) {
       arg_name <- template[[arg_type]][[i]]$key
       if (arg_name %in% names(func_meta$args)) {
-        template[[arg_type]][[i]]$description <- func_meta$args[[arg_name]]$description
-        template[[arg_type]][[i]]$defaultValue <- func_meta$args[[arg_name]]$defaultValue
-        template[[arg_type]][[i]]$paramType <-
-          args_in_template <- c(args_in_template, arg_name)
+        arg_meta <- template[[arg_type]][[i]]
+        arg_meta$description <- func_meta$args[[arg_name]]$description
+        arg_meta$defaultValue <- func_meta$args[[arg_name]]$defaultValue
+        args_in_template <- c(args_in_template, arg_name)
+        new_template[[arg_type]][[length(new_template[[arg_type]]) + 1]] <- arg_meta
       } else {
-        message(
-          glue::glue(
-            "{basename(template_filename)}: Argument {arg_name} from template not found in R function doc"
-          )
-        )
+        template_args_missing <- c(template_args_missing, arg_name)
+        if (isTRUE(keep_deprecated_args)) {
+          arg_meta <- template[[arg_type]][[i]]
+          new_template[[arg_type]][[length(new_template[[arg_type]]) + 1]] <- arg_meta
+        }
       }
     }
+  }
+
+  if (length(template_args_missing) > 0) {
+    glue::glue("{basename(template_filename)}: Argument(s) from template not found in R function doc: {paste(template_args_missing, collapse = ', ')}")
   }
 
   func_args_missing <- setdiff(names(func_meta$args), args_in_template)
@@ -88,7 +95,7 @@ update_function_template <- function(template_filename, rd_db) {
       )
     )
   }
-  return(template)
+  return(new_template)
 }
 
 #' @keywords internal
