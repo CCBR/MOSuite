7b2800dcf0e7c99bb0d69ae0310d2972bc3b518d
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-12-02 14:09:36 -0500

    fix: concat description & details

--- R/galaxy.R
+++ R/galaxy.R
@@ -6,12 +6,16 @@
 get_function_meta <- function(func_name, rd_db) {
   func_db <- rd_db[[paste0(func_name, ".Rd")]]
 
-  title <- tools:::.Rd_get_metadata(func_db, "title")
-  desc <- tools:::.Rd_get_metadata(func_db, "description")
+  title <- tools:::.Rd_get_metadata(func_db, "title") %>% trimws()
+  desc <- paste(tools:::.Rd_get_metadata(func_db, "description"),
+    tools:::.Rd_get_metadata(func_db, "details"),
+    sep = "\n\n"
+  ) %>% trimws()
   arg_desc <- dplyr::as_tibble(tools:::.Rd_get_argument_table(func_db), .name_repair = "unique_quiet")
   colnames(arg_desc) <- c("arg", "desc")
   arg_docs <- arg_desc %>%
     dplyr::pull("desc") %>%
+    trimws() %>%
     as.list()
   names(arg_docs) <- arg_desc %>% dplyr::pull("arg")
 
@@ -72,8 +76,8 @@ update_function_template <- function(template,
   abort_packages_not_installed("Rd2md")
   new_template <- list(
     r_function = template$r_function,
-    title = template$title |> Rd2md::rd_str_to_md(),
-    description = func_meta$description |> Rd2md::rd_str_to_md(),
+    title = template$title %>% Rd2md::rd_str_to_md(),
+    description = func_meta$description %>% Rd2md::rd_str_to_md(),
     columns = list(),
     inputDatasets = list(),
     parameters = list(),
@@ -86,7 +90,7 @@ update_function_template <- function(template,
       arg_name <- template[[arg_type]][[i]]$key
       if (arg_name %in% names(func_meta$args)) {
         arg_meta <- template[[arg_type]][[i]]
-        arg_meta$description <- func_args[[arg_name]]$description |> Rd2md::rd_str_to_md()
+        arg_meta$description <- func_args[[arg_name]]$description %>% Rd2md::rd_str_to_md()
         arg_meta$defaultValue <- func_args[[arg_name]]$defaultValue
         args_in_template <- c(args_in_template, arg_name)
         new_template[[arg_type]][[length(new_template[[arg_type]]) + 1]] <- arg_meta
