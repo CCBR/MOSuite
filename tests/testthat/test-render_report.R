b4fb8884efdccf20221fb5bff6b1080ac22d93ca
Kelly Sovacool <kelly.sovacool@nih.gov>
2026-07-07 15:12:51 -0400

    test: use nidap dataset for render_report()

--- tests/testthat/test-render_report.R
+++ tests/testthat/test-render_report.R
@@ -11,30 +11,6 @@ test_that("render_report runs in a temporary directory", {
       execute_params = list(
         counts_tsv = system.file(
           "extdata",
-          "RSEM.genes.expected_count.all_samples.txt.gz",
-          package = "MOSuite"
-        ),
-        samplesheet_tsv = system.file(
-          "extdata",
-          "sample_metadata.tsv.gz",
-          package = "MOSuite"
-        ),
-        group_colname = "condition",
-        label_colname = "sample_id",
-        batch_colname = "sample_id",
-        contrasts = c("knockout-wildtype")
-      )
-    )
-  )
-
-  expect_true(file.exists("report.qmd"))
-  expect_true(file.exists("report.html"))
-
-  expect_no_error(
-    render_report(
-      execute_params = list(
-        counts_tsv = system.file(
-          "extdata",
           "nidap",
           "Raw_Counts.csv.gz",
           package = "MOSuite"
@@ -44,8 +20,15 @@ test_that("render_report runs in a temporary directory", {
           "nidap",
           "Sample_Metadata_Bulk_RNA-seq_Training_Dataset_CCBR.csv.gz",
           package = "MOSuite"
-        )
+        ),
+        group_colname = "Group",
+        label_colname = "Label",
+        batch_colname = "Batch",
+        contrasts = c("B-A", "C-A", "B-C")
       )
     )
   )
+
+  expect_true(file.exists("report.qmd"))
+  expect_true(file.exists("report.html"))
 })
