d65fa8f92cff3aeec334dba2f36c3a6874a904da
Kelly Sovacool <kelly.sovacool@nih.gov>
2026-09-21 14:00:29 -0400

    ci(docker): create multi-arch mosuite-ev container
    also setup .dockerignore and improve image caching

--- docker/install_dev.R
+++ docker/install_dev.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 options(repos = c(CRAN = 'https://cloud.r-project.org'))
-install.packages('pak')
-pak::local_install_dev_deps('/opt/MOSuite', upgrade = TRUE)
+pak::local_install_dev_deps('/opt/MOSuite', upgrade = FALSE)
 dir.create('/data')
 readr::write_csv(
   tibble::as_tibble(installed.packages()),
